454a569ca7a554e7cd00240a0cf117f2ffd3b0e3
Konstantin Skrypak <konstantin.s@2muchcoffee.com>
2019-02-19 19:12:24 +0200

    change actions

--- src/actions/customer.ts
+++ src/actions/customer.ts
@@ -1,7 +1,7 @@
 import TYPES from '../types';
 import ICustomer from '../models/Customer';
 
-export const fetchCustomer = () => ({ type: TYPES.CUSTOMER.GET_CUSTOMERS_REQUEST });
+export const fetchCustomers = () => ({ type: TYPES.CUSTOMER.GET_CUSTOMERS_REQUEST });
 export const fetchCustomersSuccess = (customers: ICustomer[]) => ({
   type: TYPES.CUSTOMER.GET_CUSTOMERS_SUCCESS,
   payload: customers
@@ -14,4 +14,8 @@ export const fetchCustomerById = (id: string | number) => ({
 export const fetchCustomerByIdSuccess = (customer: ICustomer) => ({
   type: TYPES.CUSTOMER.GET_CUSTOMER_BY_ID_SUCCESS,
   payload: customer
+});
+export const setError = (error: string) => ({
+  type: TYPES.CUSTOMER.FETCH_CUSTOMER_ERROR,
+  payload: error
 });
