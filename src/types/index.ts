60088f10a102c11f928866fe32b5d20549a75e4e
Konstantin <konstantin@skrypak.dev>
2019-02-20 00:24:38 +0200

    fix types

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,6 +1,6 @@
-import * as CUSTOMER from './customer'
-import * as PRODUCT from './product'
-import * as INVOICE from './invoice'
-import * as INVOICE_ITEMS from './invoice-items'
+import * as CUSTOMER_TYPES from './customer';
+import * as PRODUCT_TYPES from './product';
+import * as INVOICE_TYPES from './invoice';
+import * as INVOICE_ITEMS_TYPES from './invoice-items';
 
-export default { CUSTOMER, PRODUCT, INVOICE, INVOICE_ITEMS };
+export { CUSTOMER_TYPES, PRODUCT_TYPES, INVOICE_TYPES, INVOICE_ITEMS_TYPES };
