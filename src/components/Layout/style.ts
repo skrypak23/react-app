05ddb129a026f1fdfda90b54d0b3f8729a1b8c53
Konstantin Skrypak <konstantin.s@2muchcoffee.com>
2019-02-19 17:14:52 +0200

    create base components

---
+++ src/components/Layout/style.ts
@@ -0,0 +1,6 @@
+const styles = {
+  container: { background: '#fff', padding: 24, minHeight: '100vh' },
+  content: { padding: '0 50px' },
+  menu: { lineHeight: '64px' }
+};
+export default styles;
---
+++ src/components/Spinner/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import {Spin} from 'antd'
+import './style.css';
+
+export default () => (
+    <div className="spinner">
+        <Spin size="large" />
+    </div>
+)
