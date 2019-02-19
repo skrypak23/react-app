979f02455dac55a5607bb348f562662ea4602b9e
Konstantin Skrypak <konstantin.s@2muchcoffee.com>
2019-02-19 17:13:35 +0200

    add layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,21 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import Routes from './routes';
+import Layout from './components/Layout';
+import store from './store';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Provider store={store}>
+        <Router>
+          <Layout>
+            <Routes />
+          </Layout>
+        </Router>
+      </Provider>
     );
   }
 }
