fa8c9db7e01096237d99f2b7053ddfa2b09df2e7
Konstantin Skrypak <konstantin.s@2muchcoffee.com>
2019-02-20 12:32:27 +0200

    fix ext

---
+++ src/actions/index.ts
@@ -0,0 +1,6 @@
+import * as CustomerActions from './customer';
+import * as ProductActions from './product';
+import * as InvoiceActions from './invoice';
+import * as InvoiceItemActions from './invoice-item';
+
+export { CustomerActions, ProductActions, InvoiceActions, InvoiceItemActions };
