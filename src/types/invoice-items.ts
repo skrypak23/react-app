335229c9ae9ce851928f341e70aacaac2a412a9c
Konstantin Skrypak <konstantin.s@2muchcoffee.com>
2019-02-20 12:29:11 +0200

    fix types

--- src/types/invoice-items.ts
+++ src/types/invoice-items.ts
@@ -1,5 +1,5 @@
-export const GET_INVOICE_ITEMS_REQUEST = 'invoice-app/customer/GET_INVOICES_ITEMS_REQUEST';
-export const GET_INVOICES_ITEMS_SUCCESS = 'invoice-app/customer/GET_INVOICE_ITEMS_SUCCESS';
-export const GET_INVOICE_ITEMS_BY_ID_REQUEST = 'invoice-app/customer/GET_INVOICE_ITEMS_BY_ID_REQUEST';
-export const GET_INVOICE_ITEMS_BY_ID_SUCCESS = 'invoice-app/customer/GET_INVOICE_ITEMS_BY_ID_SUCCESS';
-export const FETCH_INVOICE_ITEMS_ERROR = 'nvoice-app/customer/FETCH_INVOICE_ITEMS_ERROR';
+export const GET_INVOICE_ITEMS_REQUEST = 'invoice-app/invoice-item/GET_INVOICES_ITEMS_REQUEST';
+export const GET_INVOICES_ITEMS_SUCCESS = 'invoice-app/invoice-item/GET_INVOICE_ITEMS_SUCCESS';
+export const GET_INVOICE_ITEMS_BY_ID_REQUEST = 'invoice-app/invoice-item/GET_INVOICE_ITEMS_BY_ID_REQUEST';
+export const GET_INVOICE_ITEMS_BY_ID_SUCCESS = 'invoice-app/invoice-item/GET_INVOICE_ITEMS_BY_ID_SUCCESS';
+export const FETCH_INVOICE_ITEMS_ERROR = 'nvoice-app/invoice-item/FETCH_INVOICE_ITEMS_ERROR';
--- src/types/invoice.ts
+++ src/types/invoice.ts
@@ -1,5 +1,5 @@
-export const GET_INVOICES_REQUEST = 'invoice-app/customer/GET_INVOICESS_REQUEST';
-export const GET_INVOICES_SUCCESS = 'invoice-app/customer/GET_INVOICES_SUCCESS';
-export const GET_INVOICE_BY_ID_REQUEST = 'invoice-app/customer/GET_INVOICE_BY_ID_REQUEST';
-export const GET_INVOICE_BY_ID_SUCCESS = 'invoice-app/customer/GET_INVOICE_BY_ID_SUCCESS';
-export const FETCH_INVOICE_ERROR = 'nvoice-app/customer/FETCH_INVOICE_ERROR';
+export const GET_INVOICES_REQUEST = 'invoice-app/invoice/GET_INVOICESS_REQUEST';
+export const GET_INVOICES_SUCCESS = 'invoice-app/invoice/GET_INVOICES_SUCCESS';
+export const GET_INVOICE_BY_ID_REQUEST = 'invoice-app/invoice/GET_INVOICE_BY_ID_REQUEST';
+export const GET_INVOICE_BY_ID_SUCCESS = 'invoice-app/invoice/GET_INVOICE_BY_ID_SUCCESS';
+export const FETCH_INVOICE_ERROR = 'nvoice-app/invoice/FETCH_INVOICE_ERROR';
