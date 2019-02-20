84b4c1cacd1511d1fe142ae7e6194c83a9587cbd
Konstantin <konstantin@skrypak.dev>
2019-02-21 00:20:48 +0200

    fix names

--- src/api/request.ts
+++ src/api/request.ts
@@ -1,11 +1,10 @@
-async function fetchAll<T>(url: string, config: object = {}): Promise<T> {
-    try {
-      const res = await fetch(url, config);
-      return await res.json();
-    } catch (err) {
-      throw err;
-    }
+async function request<T>(url: string, config: object = {}): Promise<T> {
+  try {
+    const res = await fetch(url, config);
+    return await res.json();
+  } catch (err) {
+    throw err;
   }
-  
-  export default fetchAll;
-  
+}
+
+export default request;
--- src/epics/createDataEpic.ts
+++ src/epics/createDataEpic.ts
@@ -3,8 +3,8 @@ import { Observable, of, from } from 'rxjs';
 import { switchMap, map, catchError } from 'rxjs/operators';
 import { ofType } from 'redux-observable';
 import { CUSTOMER_TYPES, PRODUCT_TYPES, INVOICE_TYPES, INVOICE_ITEMS_TYPES } from '../types';
-import API from '../api';
 import { CustomerActions, ProductActions, InvoiceActions, InvoiceItemActions } from '../actions';
+import API from '../api';
 import ICustomer from '../models/Customer';
 import IProduct from '../models/Product';
 import IInvoice from '../models/Invoice';
@@ -18,17 +18,17 @@ const createCustomersEpic = (action$: Observable<Action>) =>
     ofType(CUSTOMER_TYPES.CREATE_CUSTOMER_REQUEST),
     switchMap((action: any) => createRequest<ICustomer>(CustomerActions, action.payload))
   );
-const fetchProductsEpic = (action$: Observable<Action>) =>
+const createProductsEpic = (action$: Observable<Action>) =>
   action$.pipe(
     ofType(PRODUCT_TYPES.CREATE_PRODUCT_REQUEST),
     switchMap((action: any) => createRequest<IProduct>(ProductActions, action.payload))
   );
-const fetchInvoicesEpic = (action$: Observable<Action>) =>
+const createInvoicesEpic = (action$: Observable<Action>) =>
   action$.pipe(
     ofType(INVOICE_TYPES.CREATE_INVOICE_REQUEST),
     switchMap((action: any) => createRequest<IInvoice>(InvoiceActions, action.payload))
   );
-const fetchInvoiceItemsEpic = (action$: Observable<Action>) =>
+const createInvoiceItemsEpic = (action$: Observable<Action>) =>
   action$.pipe(
     ofType(INVOICE_ITEMS_TYPES.CREATE_INVOICE_REQUEST),
     switchMap((action: any) => createRequest<IInvoiceItem>(InvoiceItemActions, action.payload))
@@ -49,4 +49,9 @@ function createRequest<T>(action: Actions, data: PayloadData) {
   );
 }
 
-export default [createCustomersEpic, fetchProductsEpic, fetchInvoiceItemsEpic, fetchInvoicesEpic];
+export default [
+  createCustomersEpic,
+  createProductsEpic,
+  createInvoiceItemsEpic,
+  createInvoicesEpic
+];
