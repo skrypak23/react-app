893475f33f5013a4c8253a83fd21aec437572617
Konstantin Skrypak <konstantin.s@2muchcoffee.com>
2019-02-20 12:30:01 +0200

    render table with content

--- src/pages/Customer/index.tsx
+++ src/pages/Customer/index.tsx
@@ -1,11 +1,43 @@
 import React, { useEffect, FC } from 'react';
+import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
-import Table from '../../components/Table';
+import { Table, Divider } from 'antd';
 import { CustomerActions } from '../../actions';
 import { State } from '../../reducers/customer';
 import { RootState } from '../../store/types';
+import ICustomer from '../../models/Customer';
 
 const { fetchCustomers } = CustomerActions;
+
+const columns = [
+  {
+    title: 'Name',
+    dataIndex: 'name',
+    key: 'name',
+    render: (text: string) => <Link to="javascript:;">{text}</Link>
+  },
+  {
+    title: 'Phone',
+    dataIndex: 'phone',
+    key: 'phone'
+  },
+  {
+    title: 'Address',
+    dataIndex: 'address',
+    key: 'address'
+  },
+  {
+    title: 'Action',
+    key: 'action',
+    render: () => (
+      <span>
+        <Link to="javascript:;">Edit</Link>
+        <Divider type="vertical" />
+        <Link to="javascript:;">Delete</Link>
+      </span>
+    )
+  }
+];
 
 type Props = {
   customer: State;
@@ -13,12 +45,18 @@ type Props = {
 };
 
 const Customer: FC<Props> = props => {
+  const { customer } = props;
   useEffect(() => {
     props.fetchCustomers();
-  });
+  }, []);
+
   return (
     <div>
-      <Table />
+      <Table
+        columns={columns}
+        dataSource={customer.customers as Array<ICustomer>}
+        rowKey={record => `${record.id}`}
+      />
     </div>
   );
 };
--- src/pages/Product/index.tsx
+++ src/pages/Product/index.tsx
@@ -1,3 +1,65 @@
-import React from 'react';
+import React, { useEffect, FC } from 'react';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { Table, Divider } from 'antd';
+import { ProductActions } from '../../actions';
+import { State } from '../../reducers/product';
+import { RootState } from '../../store/types';
+import IProduct from '../../models/Product';
 
-export default () => (<div>Product</div>)
+const { fetchProducts } = ProductActions;
+
+const columns = [
+  {
+    title: 'Name',
+    dataIndex: 'name',
+    key: 'name',
+    render: (text: string) => <Link to="javascript:;">{text}</Link>
+  },
+  {
+    title: 'Price',
+    dataIndex: 'price',
+    key: 'price'
+  },
+  {
+    title: 'Action',
+    key: 'action',
+    render: () => (
+      <span>
+        <Link to="javascript:;">Edit</Link>
+        <Divider type="vertical" />
+        <Link to="javascript:;">Delete</Link>
+      </span>
+    )
+  }
+];
+
+type Props = {
+  product: State;
+  fetchProducts: Function;
+};
+
+const Product: FC<Props> = props => {
+  const { product } = props;
+  useEffect(() => {
+    props.fetchProducts();
+  }, []);
+
+  return (
+    <div>
+      <Table
+        columns={columns}
+        dataSource={product.products as Array<IProduct>}
+        rowKey={record => `${record.id}`}
+      />
+    </div>
+  );
+};
+
+const mapStateToProps = (state: RootState) => ({ product: state.product });
+const mapDispatchToProps = { fetchProducts };
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Product);
