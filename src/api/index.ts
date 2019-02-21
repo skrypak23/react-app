eff44e9598552cd7b26ac7de4c633880088fcac7
Konstantin Skrypak <konstantin.s@2muchcoffee.com>
2019-02-21 19:09:40 +0200

    fix

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,10 +1,10 @@
+import fetchAll from './fetchAll';
+import request from './request';
+
 const HOST = 'http://localhost:8000';
 export const URL_ALL_CUSTOMERS = `${HOST}/api/customers`;
 export const URL_ALL_INVOICE_ITEMS = `${HOST}/api/invoice-items`;
 export const URL_ALL_INVOICES = `${HOST}/api/invoices`;
 export const URL_ALL_PRODUCTS = `${HOST}/api/products`;
-
-import fetchAll from './fetchAll';
-import request from './request';
 
 export default { fetchAll, request };
