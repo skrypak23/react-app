ff4ab565ddd640461dd85964bcce0af7fe0e8d57
Konstantin Skrypak <konstantin.s@2muchcoffee.com>
2019-02-21 15:04:24 +0200

    create content

--- src/pages/Product/Table.tsx
+++ src/pages/Product/Table.tsx
@@ -1,39 +1,42 @@
-import React, { FC } from 'react';
-import { Link } from 'react-router-dom';
-import { Table, Divider } from 'antd';
+import React, {FC} from 'react';
+import {Link} from 'react-router-dom';
+import {Table, Divider} from 'antd';
 import IProduct from '../../models/Product';
 
-const columns = [
-  {
-    title: 'Name',
-    dataIndex: 'name',
-    key: 'name',
-    render: (text: string) => <Link to="javascript:;">{text}</Link>
-  },
-  {
-    title: 'Price',
-    dataIndex: 'price',
-    key: 'price'
-  },
-  {
-    title: 'Action',
-    key: 'action',
-    render: () => (
-      <span>
-        <Link to="javascript:;">Edit</Link>
-        <Divider type="vertical" />
-        <Link to="javascript:;">Delete</Link>
-      </span>
-    )
-  }
-];
-
 type Props = {
-  data: IProduct[];
+    data: IProduct[];
+    onEdit: Function;
+    onDelete: Function
 };
 
-const ProductTable: FC<Props> = ({ data }) => (
-  <Table columns={columns} dataSource={data} rowKey={record => `${record.id}`} />
-);
+const ProductTable: FC<Props> = ({data, onEdit, onDelete}) => {
+    const renderColumns = () =>
+        [
+            {
+                title: 'Name',
+                dataIndex: 'name',
+                key: 'name',
+                render: (text: string) => <Link to="javascript:;">{text}</Link>
+            },
+            {
+                title: 'Price',
+                dataIndex: 'price',
+                key: 'price'
+            },
+            {
+                title: 'Action',
+                key: 'action',
+                render: (product: IProduct) => (
+                    <span>
+             <a onClick={() => onEdit(product.id)}>Edit</a>
+                 <Divider type="vertical"/>
+                 <a onClick={() => onDelete(product.id)}>Delete</a>
+             </span>
+                )
+            }
+        ];
+
+    return (<Table columns={renderColumns()} dataSource={data} rowKey={record => `${record.id}`}/>)
+};
 
 export default ProductTable;
--- src/pages/Product/index.tsx
+++ src/pages/Product/index.tsx
@@ -1,45 +1,69 @@
-import React, { useEffect, FC, useState } from 'react';
-import { connect } from 'react-redux';
+import React, {useEffect, useState, FC} from 'react';
+import {withRouter, RouteComponentProps} from 'react-router-dom';
+import {connect} from 'react-redux';
+import {Button, Icon} from 'antd';
 import Table from './Table';
-import { ProductForm } from '../../components/ManageForm';
-import { ProductActions } from '../../actions';
-import { State } from '../../reducers/product';
-import { RootState } from '../../store/types';
-import IProduct from '../../models/Product';
 import Drawer from '../../components/Drawer';
+import {ProductActions} from '../../actions';
+import {State} from '../../reducers/product';
+import {RootState} from '../../store/types';
+import IProduct from '../../models/Product';
+import {ProductForm} from '../../components/ManageForm';
 
-const { fetchAllProducts } = ProductActions;
+const {fetchAllProducts, fetchProductById, resetProduct, deleteProduct} = ProductActions;
 
-type Props = {
-  product: State;
-  fetchAllProducts: Function;
+type Props = RouteComponentProps<any> & {
+    product: State;
+    fetchAllProducts: Function;
+    fetchProductById: Function;
+    resetProduct: Function;
+    deleteProduct: Function;
 };
 
-const Product: FC<Props> = props => {
-  const { product } = props;
-  const [visible, changeVisible] = useState(false);
-  const [isEdit, setMode] = useState(false);
-  useEffect(() => {
-    props.fetchAllProducts();
-  }, []);
+const Product: FC<Props> = ({
+                                 fetchAllProducts,
+                                 fetchProductById,
+                                 deleteProduct,
+                                 resetProduct,
+                                 product
+                             }) => {
+    const [visible, changeVisible] = useState(false);
+    const [isEdit, setIsEdit] = useState(false);
 
-  const onClose = () => changeVisible(false);
-  const showDrawer = () => changeVisible(true);
+    useEffect(() => {
+        fetchAllProducts();
+    }, []);
 
-  return (
-    <div>
-      <Table data={product.products as Array<IProduct>} />
-      <Drawer title="Create a new customer" onClose={onClose} visible={visible}>
-        {isEdit ? null : <ProductForm />}
-      </Drawer>
-    </div>
-  );
+    const showDrawer = () => changeVisible(true);
+    const handleEdit = (id: string | number) => {
+
+        fetchProductById(id);
+        showDrawer();
+        setIsEdit(true)
+    };
+    const handleCloseForm = () => {
+        changeVisible(false);
+        setIsEdit(false);
+        resetProduct();
+    };
+
+    return (
+        <div>
+            <Button type="primary" onClick={showDrawer} htmlType="button">
+                <Icon type="plus"/> Add Product
+            </Button>
+            <Table data={product.products as Array<IProduct>} onEdit={handleEdit} onDelete={deleteProduct}/>
+            <Drawer title="Create a new product" onClose={handleCloseForm} visible={visible}>
+                <ProductForm isEdit={isEdit}/>
+            </Drawer>
+        </div>
+    );
 };
 
-const mapStateToProps = (state: RootState) => ({ product: state.product });
-const mapDispatchToProps = { fetchAllProducts };
+const mapStateToProps = (state: RootState) => ({product: state.product});
+const mapDispatchToProps = {fetchAllProducts, fetchProductById, resetProduct, deleteProduct};
 
 export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(Product);
+    mapStateToProps,
+    mapDispatchToProps
+)(withRouter(Product));
