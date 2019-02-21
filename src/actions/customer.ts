e0cde0b43efaff2fa8c1f7eb62cdfe74852012ee
Konstantin Skrypak <konstantin.s@2muchcoffee.com>
2019-02-21 13:49:25 +0200

    create some actions

--- src/actions/customer.ts
+++ src/actions/customer.ts
@@ -1,22 +1,34 @@
-import { action } from 'typesafe-actions';
+import {action} from 'typesafe-actions';
 import ICustomer from '../models/Customer';
-import { CUSTOMER_TYPES } from '../types';
-import { URL_ALL_CUSTOMERS } from '../api';
+import {CUSTOMER_TYPES} from '../types';
+import {URL_ALL_CUSTOMERS} from '../api';
 
 export const fetchAllCustomers = () =>
-  action(CUSTOMER_TYPES.GET_CUSTOMERS_REQUEST, URL_ALL_CUSTOMERS);
+    action(CUSTOMER_TYPES.GET_CUSTOMERS_REQUEST, URL_ALL_CUSTOMERS);
 export const fetchDataSuccess = (customers: ICustomer[]) =>
-  action(CUSTOMER_TYPES.GET_CUSTOMERS_SUCCESS, customers);
+    action(CUSTOMER_TYPES.GET_CUSTOMERS_SUCCESS, customers);
 
 export const fetchCustomerById = (id: string | number) =>
-  action(CUSTOMER_TYPES.GET_CUSTOMER_BY_ID_REQUEST, id);
+    action(CUSTOMER_TYPES.GET_CUSTOMER_BY_ID_REQUEST, `${URL_ALL_CUSTOMERS}/${id}`);
 export const fetchDataByIdSuccess = (customer: ICustomer) =>
-  action(CUSTOMER_TYPES.GET_CUSTOMER_BY_ID_SUCCESS, customer);
+    action(CUSTOMER_TYPES.GET_CUSTOMER_BY_ID_SUCCESS, customer);
 
-export const createCustomer = (body: ICustomer) =>
-  action(CUSTOMER_TYPES.CREATE_CUSTOMER_REQUEST, { url: URL_ALL_CUSTOMERS, body });
+export const createCustomer = (customer: ICustomer) =>
+    action(CUSTOMER_TYPES.CREATE_CUSTOMER_REQUEST, {url: URL_ALL_CUSTOMERS, body: customer});
 export const createSuccess = (customer: ICustomer) =>
-  action(CUSTOMER_TYPES.CREATE_CUSTOMER_SUCCESS, customer);
+    action(CUSTOMER_TYPES.CREATE_CUSTOMER_SUCCESS, customer);
 
+export const editCustomer = (id: string | number, customer: ICustomer) =>
+    action(CUSTOMER_TYPES.EDIT_CUSTOMER_REQUEST, {url: `${URL_ALL_CUSTOMERS}/${id}`, body: customer});
+export const editSuccess = (customer: ICustomer) =>
+    action(CUSTOMER_TYPES.EDIT_CUSTOMER_SUCCESS, customer);
+
+export const deleteCustomer = (id: string | number) =>
+    action(CUSTOMER_TYPES.DELETE_CUSTOMER_REQUEST, {url: `${URL_ALL_CUSTOMERS}/${id}`});
+export const deleteCustomerSuccess = (customer: ICustomer) =>
+    action(CUSTOMER_TYPES.DELETE_CUSTOMER_SUCCESS, customer);
+
+export const resetCustomer = () =>
+    action(CUSTOMER_TYPES.RESET_CUSTOMER, 'error');
 export const setError = (error: string) =>
-  action(CUSTOMER_TYPES.FETCH_CUSTOMER_ERROR, error);
+    action(CUSTOMER_TYPES.FETCH_CUSTOMER_ERROR, error);
