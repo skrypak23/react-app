377a672db20104c759f08fff848fdd4fd99cb966
Konstantin Skrypak <konstantin.s@2muchcoffee.com>
2019-02-21 13:38:38 +0200

    update table

--- src/pages/Customer/Table.tsx
+++ src/pages/Customer/Table.tsx
@@ -3,42 +3,51 @@ import { Link } from 'react-router-dom';
 import { Table, Divider } from 'antd';
 import ICustomer from '../../models/Customer';
 
-const columns = [
-  {
-    title: 'Name',
-    dataIndex: 'name',
-    key: 'name',
-    render: (text: string) => <Link to="javascript:;">{text}</Link>
-  },
-  {
-    title: 'Phone',
-    dataIndex: 'phone',
-    key: 'phone'
-  },
-  {
-    title: 'Address',
-    dataIndex: 'address',
-    key: 'address'
-  },
-  {
-    title: 'Action',
-    key: 'action',
-    render: () => (
-      <span>
-        <Link to="javascript:;">Edit</Link>
-        <Divider type="vertical" />
-        <Link to="javascript:;">Delete</Link>
-      </span>
-    )
-  }
-];
-
 type Props = {
   data: ICustomer[];
+  onEdit: Function;
+  onDelete: Function
 };
 
-const InvoiceTable: FC<Props> = ({ data }) => (
-  <Table columns={columns} dataSource={data} rowKey={record => `${record.id}`} />
-);
+const InvoiceTable: FC<Props> = ({ data, onEdit, onDelete }) => {
+  const renderColumns = () =>
+    [
+      {
+        title: 'Name',
+        dataIndex: 'name',
+        key: 'name',
+        render: (text: string) => <Link to="javascript:;">{text}</Link>
+      },
+      {
+        title: 'Phone',
+        dataIndex: 'phone',
+        key: 'phone'
+      },
+      {
+        title: 'Address',
+        dataIndex: 'address',
+        key: 'address'
+      },
+      {
+        title: 'Action',
+        key: 'action',
+        render: (customer: ICustomer) => (
+          <span>
+            <a onClick={() => onEdit(customer.id)}>Edit</a>
+            <Divider type="vertical" />
+            <a onClick={() => onDelete(customer.id)}>Delete</a>
+          </span>
+        )
+      }
+    ];
+
+  return (
+    <Table
+      columns={renderColumns()}
+      dataSource={data}
+      rowKey={record => `${record.id}`}
+    />
+  );
+};
 
 export default InvoiceTable;
--- src/pages/Customer/index.tsx
+++ src/pages/Customer/index.tsx
@@ -1,51 +1,68 @@
-import React, { useEffect, useState, FC } from 'react';
-import { withRouter, RouteComponentProps } from 'react-router-dom';
-import { connect } from 'react-redux';
-import { Button, Icon } from 'antd';
+import React, {useEffect, useState, FC} from 'react';
+import {withRouter, RouteComponentProps} from 'react-router-dom';
+import {connect} from 'react-redux';
+import {Button, Icon} from 'antd';
 import Table from './Table';
 import Drawer from '../../components/Drawer';
-import { CustomerActions } from '../../actions';
-import { State } from '../../reducers/customer';
-import { RootState } from '../../store/types';
+import {CustomerActions} from '../../actions';
+import {State} from '../../reducers/customer';
+import {RootState} from '../../store/types';
 import ICustomer from '../../models/Customer';
-import { CustomerForm } from '../../components/ManageForm';
+import {CustomerForm} from '../../components/ManageForm';
 
-const { fetchAllCustomers } = CustomerActions;
+const {fetchAllCustomers, fetchCustomerById, resetCustomer, deleteCustomer} = CustomerActions;
 
 type Props = RouteComponentProps<any> & {
-  customer: State;
-  fetchAllCustomers: Function;
+    customer: State;
+    fetchAllCustomers: Function;
+    fetchCustomerById: Function;
+    resetCustomer: Function;
+    deleteCustomer: Function;
 };
 
-const Customer: FC<Props> = props => {
-  const { customer } = props;
-  const [visible, changeVisible] = useState(false);
-  const [isEdit, setMode] = useState(false);
+const Customer: FC<Props> = ({
+                                 fetchAllCustomers,
+                                 fetchCustomerById,
+                                 deleteCustomer,
+                                 resetCustomer,
+                                 customer
+                             }) => {
+    const [visible, changeVisible] = useState(false);
+    const [isEdit, setIsEdit] = useState(false);
 
-  useEffect(() => {
-    props.fetchAllCustomers();
-  }, []);
+    useEffect(() => {
+        fetchAllCustomers();
+    }, []);
 
-  const onClose = () => changeVisible(false);
-  const showDrawer = () => changeVisible(true);
+    const showDrawer = () => changeVisible(true);
+    const handleEdit = (id: string | number) => {
+        fetchCustomerById(id);
+        showDrawer();
+        setIsEdit(true)
+    };
+    const handleCloseForm = () => {
+        changeVisible(false);
+        setIsEdit(false);
+        resetCustomer();
+    };
 
-  return (
-    <div>
-      <Button type="primary" onClick={showDrawer}>
-        <Icon type="plus" /> Add Customer
-      </Button>
-      <Table data={customer.customers as Array<ICustomer>} />
-      <Drawer title="Create a new customer" onClose={onClose} visible={visible}>
-        {isEdit ? null : <CustomerForm />}
-      </Drawer>
-    </div>
-  );
+    return (
+        <div>
+            <Button type="primary" onClick={showDrawer} htmlType="button">
+                <Icon type="plus"/> Add Customer
+            </Button>
+            <Table data={customer.customers as Array<ICustomer>} onEdit={handleEdit} onDelete={deleteCustomer}/>
+            <Drawer title="Create a new customer" onClose={handleCloseForm} visible={visible}>
+                <CustomerForm isEdit={isEdit}/>
+            </Drawer>
+        </div>
+    );
 };
 
-const mapStateToProps = (state: RootState) => ({ customer: state.customer });
-const mapDispatchToProps = { fetchAllCustomers };
+const mapStateToProps = (state: RootState) => ({customer: state.customer});
+const mapDispatchToProps = {fetchAllCustomers, fetchCustomerById, resetCustomer, deleteCustomer};
 
 export default connect(
-  mapStateToProps,
-  mapDispatchToProps
+    mapStateToProps,
+    mapDispatchToProps
 )(withRouter(Customer));
