624732122edeabd27535f3e4c648c66f2ac4d81e
Konstantin Skrypak <konstantin.s@2muchcoffee.com>
2019-03-01 18:37:11 +0200

    deleted helper funcs

--- src/shared/utils/index.ts
+++ src/shared/utils/index.ts
@@ -1,28 +1,13 @@
-import { InvoiceItemRequest }  from '../../redux/request/actions';
+import { InvoiceItemRequest } from '../../redux/request/actions';
 import IInvoiceItem from '../models/InvoiceItem';
 import { filter, map } from 'rxjs/operators';
 import { from } from 'rxjs';
 
-export const filteredData = (data: ReadonlyArray<any>, id: number) =>
-  data.filter(fD => fD.id !== id);
-export const filteredDataByIdx = (data: ReadonlyArray<any>, id: number) =>
-  data.filter((_, idx) => idx !== id);
-export const mapedDataByIdx = (data: ReadonlyArray<any>, payloadData: any) =>
-  data.map((mD, idx) => {
-    if (idx === payloadData.id) return payloadData;
-    return mD;
-  });
-export const mapedData = (data: ReadonlyArray<any>, payloadData: any) =>
-  data.map(mD => {
-    if (mD.id === payloadData.id) return payloadData;
-    return mD;
-  });
-
 export const createItems = (state$: any, action: any) => {
-  return from(state$.value.invoiceItem.invoiceItems).pipe(
-    filter((invoiceItem: any) => !invoiceItem.hasOwnProperty('id')),
+  return from(state$.value.invoiceItem.entities).pipe(
+    filter(invoiceItem => !invoiceItem.hasOwnProperty('id')),
     map(item =>
-      InvoiceItemRequest.CreateInvoiceItemActions.createInvoiceItem(action.payload.id, {
+      InvoiceItemRequest.Action.createInvoiceItem(action.payload.id, {
         ...item
       } as IInvoiceItem)
     )
